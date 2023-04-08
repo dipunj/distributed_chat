193370ea12968fa2bd9ed6e6b552d68cb09817b2
Jim Tomko <j.tomko.175@gmail.com>
2023-04-08 13:05:33 -0400

    Adds conversion to and from database format.
    We'll store the vector clock as a single string in the database.
    This commit adds functions to convert between that format and the
    VectorClock struct.

--- src/server/network/vector_clock.go
+++ src/server/network/vector_clock.go
@@ -1,15 +1,22 @@
 package network
 
+import (
+	"strconv"
+	"strings"
+)
+
+const NUM_REPLICAS = 5
+
 type VectorClock struct {
 	my_id  int
-	clocks [5]int
+	clocks [NUM_REPLICAS]int
 }
 
-func (vc *VectorClock) increment() {
+func (vc *VectorClock) Increment() {
 	vc.clocks[vc.my_id] += 1
 }
 
-func (vc *VectorClock) update_from(other VectorClock) {
+func (vc *VectorClock) UpdateFrom(other VectorClock) {
 	for i := range vc.clocks {
 		if i == vc.my_id {
 			continue
@@ -19,4 +26,29 @@ func (vc *VectorClock) update_from(other VectorClock) {
 			vc.clocks[i] = other.clocks[i]
 		}
 	}
+}
+
+func (vc *VectorClock) ToDbFormat() string {
+	var clock_strings [NUM_REPLICAS]string
+
+	for i := range clock_strings {
+		clock_strings[i] = strconv.Itoa(vc.clocks[i])
+	}
+
+	return strings.Join(clock_strings[:], ",")
+}
+
+func FromDbFormat(db_str string) VectorClock {
+	var clock_strings = strings.Split(db_str, ",")
+	var clocks [NUM_REPLICAS]int
+	for i := range clocks {
+		c, err := strconv.Atoi(clock_strings[i])
+		if err != nil {
+			clocks[i] = -1
+		} else {
+			clocks[i] = c
+		}
+	}
+
+	return VectorClock{my_id: -1, clocks: clocks}
 }
