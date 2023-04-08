209ee111733180b81c7667b5e75628db54f6a900
Dipunj Gupta <zero@dipunj.com>
2023-04-08 10:36:38 -0400

    stores ports as strings

--- src/server/server.go
+++ src/server/server.go
@@ -11,9 +11,9 @@ const MAX_CHAR_PER_LINE = 80
 
 const (
 	// listen on all interfaces
-	DEFAULT_INTERFACE        = "0.0.0.0"
-	DEFAULT_CLIENT_PORT      = 12000
-	DEFAULT_REPLICATION_PORT = 11000
+	DEFAULT_INTERFACE     = "0.0.0.0"
+	DEFAULT_PUBLIC_PORT   = "12000"
+	DEFAULT_INTERNAL_PORT = "11000"
 )
 
 func GetServerID() int {
@@ -27,8 +27,8 @@ func main() {
 
 	server_id := GetServerID()
 
-	client_serve_address := DEFAULT_INTERFACE + ":" + string(DEFAULT_CLIENT_PORT)
-	replication_serve_address := DEFAULT_INTERFACE + ":" + string(DEFAULT_REPLICATION_PORT)
+	client_serve_address := DEFAULT_INTERFACE + ":" + DEFAULT_PUBLIC_PORT
+	replication_serve_address := DEFAULT_INTERFACE + ":" + DEFAULT_INTERNAL_PORT
 
 	db.ConnectToDB()
 	defer db.TerminateDBConn()
